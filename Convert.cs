86f3f61e3c3c045bc7d3fc4b224c3b6e60648270
VJ (Velauther) Vijayaruban <vj@getcape.io>
2023-01-06 17:21:13 +0000

    clean up

---
+++ Convert.cs
@@ -0,0 +1,38 @@
+﻿namespace NumberToRomanNumeral;
+
+public class Convert
+{
+    public static readonly IReadOnlyDictionary<int, string> RomanNumeralsDenominations = new Dictionary<int, string>
+    {
+        {1, "I"},
+        {5, "V"},
+        {10, "X"},
+        {50, "L"},
+        {100, "C"},
+        {500, "D"},
+        {1000, "M"}
+    };
+
+    //public static Stack<int> OrderDenominationsStacks { get; } =
+    //    new(RomanNumeralsDenominations.OrderBy(x => x.Key).Select(x => x.Key));
+
+    public static string ToRomanNumeral(int number)
+    {
+        if (number <= 0)
+            throw new InvalidDataException("Input should be a Natural Number to convert it to a Roman Numeral.");
+        if (RomanNumeralsDenominations.TryGetValue(number, out var value))
+        {
+            return value;
+        }
+
+        //var pop = OrderDenominationsStacks.Pop();
+        //if (number > pop)
+        //{
+        //    var quotient = number / pop;
+        //    var remainder = number % pop;
+
+        //}
+
+        return "";
+    }
+}
--- NumberToRomanNumeralSpec.cs
+++ NumberToRomanNumeralSpec.cs
@@ -3,55 +3,6 @@ using Xunit;
 
 namespace NumberToRomanNumeral;
 
-/*
- * Write code to convert any number between 1 and 2000 to a Roman Numeral, using the standard rules.
- * You can use libraries/packages within reason but your solution must demonstrate the application of the rules logic you have written.
- * Your solution will be assessed on whether it: -
- *
-    1) solves the problem
-    2) is well structured
-    3) has an appropriate level of complexity
-    Feel free to use tools you are comfortable with, but you must use C# as the main language. When you are done provide access to the solution so that the interview panel may assess it in its entirety.
-    Note any code you develop is your code. It will not be copied or reused / repurposed within XPS.  
- */
-
-public class Convert
-{
-    public static readonly IReadOnlyDictionary<int, string> RomanNumeralsDenominations = new Dictionary<int, string>
-    {
-        {1, "I"},
-        {5, "V"},
-        {10, "X"},
-        {50, "L"},
-        {100, "C"},
-        {500, "D"},
-        {1000, "M"}
-    };
-
-    public static Stack<int> OrderDenominationsStacks { get; } =
-        new(RomanNumeralsDenominations.OrderBy(x => x.Key).Select(x => x.Key));
-
-    public static string ToRomanNumeral(int number)
-    {
-        if (number <= 0)
-            throw new InvalidDataException("Input should be a Natural Number to convert it to a Roman Numeral.");
-        if (RomanNumeralsDenominations.TryGetValue(number, out var value))
-        {
-            return value;
-        }
-
-        var pop = OrderDenominationsStacks.Pop();
-        if (number > pop)
-        {
-            var quotient = number / pop;
-            var remainder = number % pop;
-
-        }
-
-        return "";
-    }
-}
-
 public class NumberToRomanNumeralSpec
 {
     [Fact]
