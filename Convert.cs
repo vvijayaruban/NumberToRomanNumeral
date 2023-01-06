2c051c8bc6a8e92270c860240f3ef871b1063247
VJ (Velauther) Vijayaruban <vj@getcape.io>
2023-01-06 17:43:02 +0000

    add Individual Decimal Places table

--- Convert.cs
+++ Convert.cs
@@ -15,6 +15,64 @@ public class Convert
         {1000, "M"}
     };
 
+
+    public static readonly IReadOnlyDictionary<string, IReadOnlyDictionary<int, string>> IndividualDecimalPlaces =
+        new Dictionary<string, IReadOnlyDictionary<int, string>>
+        {
+            {
+                DecimalPlaces.Thousands, new Dictionary<int, string>
+                {
+                    {1, "M"},
+                    {2, "MM"}
+                }
+            },
+            {
+                DecimalPlaces.Hundreds, new Dictionary<int, string>
+                {
+                    {1, "C"},
+                    {2, "CC"},
+                    {3, "CCC"},
+                    {4, "CD"},
+                    {5, "D"},
+                    {6, "DC"},
+                    {7, "DCC"},
+                    {8, "DCCC"},
+                    {9, "CM"}
+                }
+            }
+            ,
+            {
+                DecimalPlaces.Tens, new Dictionary<int, string>
+                {
+                    {1, "X"},
+                    {2, "XX"},
+                    {3, "XXX"},
+                    {4, "XL"},
+                    {5, "L"},
+                    {6, "DC"},
+                    {7, "DCC"},
+                    {8, "DCCC"},
+                    {9, "CM"}
+                }
+            }
+            ,
+            {
+                DecimalPlaces.Units, new Dictionary<int, string>
+                {
+                    {1, "I"},
+                    {2, "II"},
+                    {3, "III"},
+                    {4, "IV"},
+                    {5, "V"},
+                    {6, "VI"},
+                    {7, "VII"},
+                    {8, "VIII"},
+                    {9, "IX"}
+                }
+            }
+        };
+
+
     public static string ToRomanNumeral(int number)
     {
         ValidateInput(number);
@@ -44,5 +102,10 @@ public class Convert
     private static bool IsNaturalNumber(int number)
     {
         return number > 0;
+    }
+
+    private static class DecimalPlaces
+    {
+        public const string Thousands = "Thousands", Hundreds = "Hundreds",Tens = "Tens",Units = "Units";
     }
 }
